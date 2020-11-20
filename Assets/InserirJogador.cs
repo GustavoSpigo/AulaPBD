b1a9592e000f9bb59bc2ad358785c8269f941233
Gustavo Gomes <gustavo@spigo.net>
2020-11-20 20:06:40 -0300

    Validação de insert

--- Assets/InserirJogador.cs
+++ Assets/InserirJogador.cs
@@ -10,18 +10,50 @@ public class InserirJogador : MonoBehaviour
     public InputField inputSenha;
     public InputField inputEmail;
     public Dropdown inputGenero;
+
+    public Text msgErro;
     public void Inserir()
     {
-        StartCoroutine(inserirJogador());
+        if (ValidacaoLocal())
+        {
+            StartCoroutine(VerificaSeExisteNick());
+        }
+        
     }
+    IEnumerator VerificaSeExisteNick()
+    {
+        WWWForm wwwf = new WWWForm();
+        wwwf.AddField("nick", inputNick.text);
+        wwwf.AddField("email", inputEmail.text);
 
+        using (var w = UnityWebRequest.Post("http://localhost/jogos/verificaNickEmail.php", wwwf))
+        {
+            yield return w.SendWebRequest();
+            if (w.isNetworkError || w.isHttpError)
+            {
+                Debug.Log(w.error);
+            }
+            else
+            {
+                if (w.downloadHandler.text.Equals("Nick e Email nao repete"))
+                {
+                    StartCoroutine(inserirJogador());
+                }
+                else
+                {
+                    //deu erro
+                }
+
+            }
+        }
+    }
     IEnumerator inserirJogador()
     {
         WWWForm wwwf = new WWWForm();
         wwwf.AddField("nick", inputNick.text);
         wwwf.AddField("email", inputEmail.text);
         wwwf.AddField("senha", MD5.Md5Sum(inputSenha.text));
-        wwwf.AddField("genero", inputGenero.value);
+        wwwf.AddField("genero", inputGenero.options[inputGenero.value].text);
 
         using (var w = UnityWebRequest.Post("http://localhost/jogos/inserirJogador.php", wwwf))
         {
@@ -44,5 +76,25 @@ public class InserirJogador : MonoBehaviour
                 
             }
         }
+    }
+
+    private bool ValidacaoLocal()
+    {
+        if (inputNick.text.Trim().Length < 3)
+        {
+            msgErro.text = "O nick precisa ter 3 ou mais caracteres";
+            return false;
+        }
+        if(inputNick.text.Trim().Contains(" "))
+        {
+            msgErro.text = "O nick não pode conter espaços";
+            return false;
+        }
+        if (inputEmail.text.Trim().Length == 0)
+        {
+            msgErro.text = "Informe o e-mail";
+            return false;
+        }
+        return true;
     }
 }
